251f3a81c4cc64841173a3c5dde528a28578a920
David Bogaerts <dbogaerts@gmail.com>
2019-02-11 18:46:59 -0600

    login code changes and base login css

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './index.css';
 
 // for logins, once tenant hits upload, property is switched to 
 //read only. subsequent logins by tenenat will only display prior
@@ -31,42 +31,54 @@ class Login extends Component {
 
     console.log(this.state);
     return(
-      <div>
-        <section>
+      <div className="login">
+        <section className="login-section">
 
           <form>
             <h4>Tenant Login</h4>
             <label>
               Username:
+              <br />
               <input type="text" onChange={this.handleChange} value={this.state.tenantUsername} name="tenantUsername" />
             </label>
+              <br />
             <label>
               Password:
+              <br />
               <input type="password" onChange={this.handleChange} value={this.state.tenantPassword} name="tenantPassword" />
             </label>
+              <br />
             <label>
               Property Code:
+              <br />
               <input type="text" onChange={this.handleChange} value={this.state.tenantPropertyCode} name="tenantPropertyCode" />
             </label>
+              <br />
             <input type="hidden" name="userDesignation" value="tenant" />
+              <br />
             <input type="Submit" />
           </form>
 
         </section>
-        <hr />
-        <section>
+        
+        <section className="login-section">
           
           <form>
             <h4>Landlord Login</h4>
             <label>
               Username:
+              <br />
               <input type="text" onChange={this.handleChange} value={this.state.landlordUsername} name="landlordUsername" />
             </label>
+              <br />
             <label>
               Password:
+              <br />
               <input type="password" onChange={this.handleChange} value={this.state.landlordPassword} name="landlordPassword" />
             </label>
+              <br />
             <input type="hidden" name="userDesignation" value="landlord" />
+              <br />
             <input type="Submit" />
           </form>
 
@@ -77,4 +89,21 @@ class Login extends Component {
 }
 
 export default Login;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
